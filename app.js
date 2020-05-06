b0a8716c372d7d6ad20b9041309d173f055dd690
mansmahamat <mansour.mahamat.salle@gmail.com>
2020-05-06 13:16:07 +0200

    add homepage, products page, creating product page  and stylize it with css

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser'); 
 
+const app = express();
 
-const server = http.createServer(routes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
