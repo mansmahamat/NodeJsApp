1740dfee6e040a57faca317bd9b1260b1c31108d
mansmahamat <mansour.mahamat.salle@gmail.com>
2020-05-18 16:42:56 +0200

    project store

--- routes/admin.js
+++ routes/admin.js
@@ -2,21 +2,16 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
 
-router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
 
-router.post('/add-product',(req, res, next) => {
-    products.push({ title : req.body.title });
-    console.log(products);
-    res.redirect('/');
-});
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct );
 
-exports.routes = router;
-exports.products = products;
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct );
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
-const adminData = require('./admin');
+
+
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', productsController.getProducts );
 
 module.exports = router;
