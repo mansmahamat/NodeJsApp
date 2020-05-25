c4f3abd4d482ea131d9ec6ea241b9d48e44e9ab1
mansmahamat <mansour.mahamat.salle@gmail.com>
2020-05-25 23:58:16 +0200

    add delete function

--- models/cart.js
+++ models/cart.js
@@ -47,9 +47,12 @@ module.exports = class Cart {
                 return;
             }
             const updatedCart = {...JSON.parse(fileContent)};
-            const product = updatedCart.products.findIndex(prod.id === id); 
+            const product = updatedCart.products.find(prod => prod.id === id); 
+            if (!product) {
+                return;
+            }
             const productQty = product.qty;
-            updatedCart.products = updatedCart.product.filter(
+            updatedCart.products = updatedCart.products.filter(
                 prod => prod.id !== id
             );
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
